6af5b650ea3d94bc650712266f356aa72f04f165
gitbchang <brian.h.chang1@gmail.com>
2017-02-25 13:12:07 -0600

    session handling with access token

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,43 +1,65 @@
-$(document).ready(function() {
-
-  var profile1;
-
-  var options = {
-  rememberLastLogin: true,
-  autoclose: true,
-  auth: {
-    redirect: false
-  }
-};
-  
-  var lock = new Auth0Lock('w0afrsXkjEvuLPkJAmX7iI3GhWDVKTFB', 'bchang55.auth0.com', options, {
-    auth: {
-      params: { scope: 'openid name email'  } //Details: https://auth0.com/docs/scopes
-    }
-  });
-
+$(document).ready(function () {
   // hide logout button on initial load
   $('.btn-logout').hide();
   $('.avatar').hide();
 
-  $('.btn-login').click(function(e) {
+  var lock = new Auth0Lock('w0afrsXkjEvuLPkJAmX7iI3GhWDVKTFB', 'bchang55.auth0.com', options, {
+    auth: {
+      params: {
+        scope: 'openid name email'
+      } //Details: https://auth0.com/docs/scopes
+    }
+  });
+
+  // Check if there is an existing access token
+
+  var access_token = localStorage.getItem('access_token');
+  // log user in, if there is a token
+  if (null != access_token) {
+    lock.getUserInfo(access_token, function (err, profile) {
+      if (err) {
+        // Remove expired token (if any) from localStorage
+        localStorage.removeItem('access_token');
+        return alert('There was an error getting the profile: ' + err.message);
+      } // 
+      else {
+        retrieve_profile();
+        console.log("user authenticated");
+      }
+    });
+  }
+
+
+  var options = {
+    rememberLastLogin: true,
+    autoclose: true,
+    auth: {
+      redirect: false
+    }
+  };
+
+  
+
+
+
+  $('.btn-login').click(function (e) {
     e.preventDefault();
     console.log('hello');
     lock.show();
   });
 
-  $('.btn-logout').click(function(e) {
+  $('.btn-logout').click(function (e) {
     e.preventDefault();
     logout();
   })
 
-  lock.on("authenticated", function(authResult) {
-    lock.getUserInfo(authResult.accessToken, function(error, profile) {
+  lock.on("authenticated", function (authResult) {
+    lock.getUserInfo(authResult.accessToken, function (error, profile) {
       if (error) {
         // Handle error
         return;
       }
-      // profile1 = profile;
+
       localStorage.setItem('access_token', authResult.accessToken);
       // Display user information
       show_profile_info(profile);
@@ -45,7 +67,7 @@ $(document).ready(function() {
   });
 
   //retrieve the profile:
-  var retrieve_profile = function() {
+  var retrieve_profile = function () {
     var access_token = localStorage.getItem('accessToken');
     if (access_token) {
       lock.getUserInfo(access_token, function (err, profile) {
@@ -59,17 +81,16 @@ $(document).ready(function() {
     }
   };
 
-  var show_profile_info = function(profile) {
-    profile1 = profile;
-     $('.nickname').text(profile.nickname);
-     $('.nickname').show();
-     $('.btn-login').hide();
-     $('.avatar').attr('src', profile.picture).show();
-     $('.btn-logout').show();
+  var show_profile_info = function (profile) {
+    $('.nickname').text(profile.nickname);
+    $('.nickname').show();
+    $('.btn-login').hide();
+    $('.avatar').attr('src', profile.picture).show();
+    $('.btn-logout').show();
   };
 
-  var logout = function() {
-    localStorage.removeItem('id_token');
+  var logout = function () {
+    localStorage.removeItem('access_token');
     window.location.href = "/";
   };
 
