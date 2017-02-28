f44a88c481eadc64fa991c63918d3de7e61fd292
gitbchang <brian.h.chang1@gmail.com>
2017-02-28 17:46:46 -0600

    cleaned up files

--- models/usermodel.js
+++ models/usermodel.js
@@ -21,6 +21,10 @@ module.exports = function(sequelize, DataTypes){
         },
         password: {
             type: DataTypes.STRING
+        },
+        admin: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: true
         }
     },
     {
