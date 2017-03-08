0cbef9c27a1fe9971d1526a5181cc5abf514ce62
Bob Deckard <bdeckard92@gmail.com>
2017-03-07 19:54:54 -0600

    posting and getting with mysql using handlbars

--- handlebars_reservations/models/todo.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-  var Reservation = sequelize.define("Reservation", {
-    name: DataTypes.STRING,
-    phone: DataTypes.STRING,
-    email: DataTypes.STRING,
-    date: DataTypes.DATE
-  });
-  return Reservation;
-};
--- handlebars_reservations/server.js
+++ handlebars_reservations/server.js
@@ -9,16 +9,19 @@ var mysql = require("mysql");
 // Sets up the Express App
 // =============================================================
 var app = express();
+var port = 8080;
+
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({
 	extended: false
-}))
+}));
+
 app.use(methodOverride('_method'));
 app.engine('handlebars', exphbs({defaultLayout: "main"}));
 app.set('view engine', 'handlebars');
 
-var port = 8080;
-app.listen(port);
-console.log("listening on port " +port);
+
+
 
 var connection = mysql.createConnection({
 	host: 'localhost',
@@ -29,50 +32,31 @@ var connection = mysql.createConnection({
 connection.connect(function(err){
 	if(err)throw err;
 	console.log('Connected as id: '+connection.threadId)
-})
+});
 //gets info from database to send to handlebar ul on index.handlebars this works
 app.get("/",function(req,res){
 	connection.query("SELECT name, date FROM reservations;", function(err,data){
 		res.render('index',{reservations:data});
 	})
 
-})
+});
+
+
+
 //hopefully inserts a new reservation into database but can't get it to work
 app.post('/create', function(req, res){
-	connection.query('INSERT INTO reservations (name, phone, email, date) VALUES(?);', [
-		res.body.name, 
-		res.body.phone, 
-		res.body.email, 
-		res.body.date
+	console.log(req.body);
+	connection.query('INSERT INTO reservations (name, phone, email, date) VALUES(?,?,?,?);', [
+		req.body.name, 
+		req.body.phone, 
+		req.body.email, 
+		req.body.date
 		], function(err,result){
 			if(err)throw err;
 			res.redirect('/');
 		})
 
 });
-/*var PORT = process.env.PORT || 8080;
 
-// Requiring our models for syncing
-var db = require("./models");
-
-// Sets up the Express app to handle data parsing
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-
-// Static directory
-app.use(express.static("./public"));
-
-// Routes =============================================================
-
-require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
-
-// Syncing our sequelize models and then starting our express app
-db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
-});
-*/
+app.listen(port);
+console.log("listening on port " +port);
